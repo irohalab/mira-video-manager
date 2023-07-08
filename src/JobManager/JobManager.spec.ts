d8db70c232f305259b8943421f2fb2f1c155bb32
Everett Summer <everett_summer@iroha.io>
2023-07-08 14:16:50 +0800

    fix test spec, bump version to 1.4.0 (#49)

--- src/JobManager/JobManager.spec.ts
+++ src/JobManager/JobManager.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 IROHA LAB
+ * Copyright 2023 IROHA LAB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 /* tslint:disable:no-string-literal */
 import test from 'ava';
+import 'reflect-metadata';
 import { Container, interfaces } from 'inversify';
 import { ConfigManager } from '../utils/ConfigManager';
 import { Sentry, TYPES } from '@irohalab/mira-shared';
--- src/utils/ConditionParser.spec.ts
+++ src/utils/ConditionParser.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 IROHA LAB
+ * Copyright 2023 IROHA LAB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 
 import test from 'ava';
+import 'reflect-metadata';
 import { ConditionParser } from './ConditionParser';
 
 test('detect invalid identifier, keyword and punctuator', t => {
